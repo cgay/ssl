e6ff83b5a004ba1ec88acf43adb6f7af36a10ffb
Carl Gay <carlgay@gmail.com>
2026-08-17 20:11:39 -0400

    Use OD v2026.2.0 in test workflow

--- sources/support.c
+++ sources/support.c
@@ -6,9 +6,9 @@ copyright: Original Code is Copyright (c) 2010 Dylan Hackers;
 License:   See License.txt in this distribution for details.
 Warranty:  Distributed WITHOUT WARRANTY OF ANY KIND
 */
-#include <openssl/ssl.h>
-#include <openssl/pem.h>
-#include <openssl/x509.h>
+#include "openssl/ssl.h"
+#include "openssl/pem.h"
+#include "openssl/x509.h"
 
 long my_SSL_set_mode (SSL* s, long op) {
   return SSL_set_mode(s, op);
